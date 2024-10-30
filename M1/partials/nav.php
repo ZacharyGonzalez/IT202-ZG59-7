041d287ccf005ec7ccabbac6e3b35d15ffeffa60
Zack G <zg59@njit.edu>
2024-10-30 17:43:20 -0400

    PUSHING NEW STUFF FOR A NEW DAY

--- M1/partials/nav.php
+++ M1/partials/nav.php
@@ -24,6 +24,7 @@ require_once(__DIR__ . "/../lib/functions.php");
 
 ?>
 <nav>
+    
     <ul>
         <?php if (is_logged_in()) : ?>
             <li><a href="home.php">Home</a></li>
@@ -41,4 +42,29 @@ require_once(__DIR__ . "/../lib/functions.php");
 
         <?php endif; ?>
     </ul>
+    <style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #111;
+}
+</style>
 </nav>
